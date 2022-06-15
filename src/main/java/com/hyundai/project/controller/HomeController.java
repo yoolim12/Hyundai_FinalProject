cc574229b61278afd993df59b8600325ea45d9d4
minketmon <gjm1026@naver.com>
2022-06-15 16:15:40 +0900

    header

--- src/main/java/com/hyundai/project/controller/HomeController.java
+++ src/main/java/com/hyundai/project/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	
 	@GetMapping("/home")
 	public String main() {
-		return "fragments/header";
+		return "/home";
 	}// end insert
 	
 	@GetMapping("/loginForm")
---
+++ src/main/java/com/hyundai/project/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.hyundai.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+/* 작성자 : 문혁
+ * 메인 페이지로 이동시키는 컨트롤러
+ */
+@Log4j2
+@Controller
+public class MainController {
+	
+	@GetMapping("/main")
+	public String main() {
+		log.info("메인페이지 요청");
+		return "/main";
+	}
+}
