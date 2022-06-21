e73cee500484b5386e5fc4678668594ee5df8693
minketmon <gjm1026@naver.com>
2022-06-22 08:59:52 +0900

    update

--- src/main/java/com/hyundai/project/controller/MypageRestController.java
+++ src/main/java/com/hyundai/project/controller/MypageRestController.java
@@ -28,26 +28,26 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequestMapping("/mypage")
 public class MypageRestController {
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private MemberDAO memberDAO;
 
 	@Autowired
 	private MemberService service;
-	
+
 	@RequestMapping(value="/passwordCheck", method=RequestMethod.POST)
 	@ResponseBody
-	public void passwordCheck(@RequestBody HashMap<String, String> map, 
-			@AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) throws IOException {
+	public void passwordCheck(@RequestBody HashMap<String, String> map,
+							  @AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) throws IOException {
 		String mpassword = map.get("mpassword");
 
 		if(passwordEncoder.matches(mpassword, memberDAO.findByEmail(authMemberDTO.getEmail(), 0).getMpassword())) {
 			Gson gson = new Gson();
-    		response.setContentType("application/json; charset=utf-8");
-    		response.getWriter().print(gson.toJson(map));
+			response.setContentType("application/json; charset=utf-8");
+			response.getWriter().print(gson.toJson(map));
 			System.out.println("success!!");
 		}
 		else {
@@ -55,16 +55,16 @@ public class MypageRestController {
 			System.out.println("fail!!");
 		}
 	}
-	
+
 	@RequestMapping(value="/passwordCompare")
 	@ResponseBody
-	public void passwordCompare(@RequestBody HashMap<String, String> map, 
-			@AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) throws IOException {
+	public void passwordCompare(@RequestBody HashMap<String, String> map,
+								@AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) throws IOException {
 		String oldpasswordinput = map.get("oldpassword");
 		if(passwordEncoder.matches(oldpasswordinput, memberDAO.findByEmail(authMemberDTO.getEmail(), 0).getMpassword())) {
 			Gson gson = new Gson();
-    		response.setContentType("application/json; charset=utf-8");
-    		response.getWriter().print(gson.toJson(map));
+			response.setContentType("application/json; charset=utf-8");
+			response.getWriter().print(gson.toJson(map));
 			System.out.println("success!!");
 		}
 		else {
@@ -72,24 +72,23 @@ public class MypageRestController {
 			System.out.println("fail!!");
 		}
 	}
-	
-	@RequestMapping(value="/modifyPassword")
-	@ResponseBody
-	public void modifyPassword(@RequestBody HashMap<String, String> map, 
-			@AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) {
-		String mpassword = passwordEncoder.encode(map.get("mpassword"));
-		MemberJoinDTO memberJoinDTO = memberDAO.findByEmail(authMemberDTO.getEmail(), 0);
-		Date birth = memberJoinDTO.getBirth();
-		
-		try {
-    		service.modifyMember(memberJoinDTO.getMemail(), mpassword, memberJoinDTO.getMname(), birth, null, null, memberJoinDTO.getGno());
-    		
-        	Gson gson = new Gson();
-    		response.setContentType("application/json; charset=utf-8");
-    		response.getWriter().print(gson.toJson(map)); 
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
-	}
-}
+
+//	@RequestMapping(value="/modifyPassword")
+//	@ResponseBody
+//	public void modifyPassword(@RequestBody HashMap<String, String> map,
+//			@AuthenticationPrincipal AuthMemberDTO authMemberDTO, HttpServletResponse response) {
+//		String mpassword = passwordEncoder.encode(map.get("mpassword"));
+//		MemberJoinDTO memberJoinDTO = memberDAO.findByEmail(authMemberDTO.getEmail(), 0);
+//		Date birth = memberJoinDTO.getBirth();
+//
+//		try {
+//    		service.modifyMember(memberJoinDTO.getMemail(), mpassword, memberJoinDTO.getMname(), birth, null, null, memberJoinDTO.getGno());
+//
+//        	Gson gson = new Gson();
+//    		response.setContentType("application/json; charset=utf-8");
+//    		response.getWriter().print(gson.toJson(map));
+//    	}catch(Exception e){
+//    		e.printStackTrace();
+//    	}
+//	}
 }
--- src/main/java/com/hyundai/project/service/MemberServiceImpl.java
+++ src/main/java/com/hyundai/project/service/MemberServiceImpl.java
@@ -1,45 +1,38 @@
 package com.hyundai.project.service;
 
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hyundai.project.dto.MemberDTO;
-import com.hyundai.project.memberDAO.MemberDAO;
+import com.hyundai.project.dto.MainListVO;
+import com.hyundai.project.dto.MainMagazineListVO;
+import com.hyundai.project.productDAO.MainDAO;
 
 import lombok.extern.log4j.Log4j2;
-
+/* 작성자 :  문혁
+ * 메인 페이지에 관련된 DB 호출을 관리하는 클래스
+ */
 @Service
 @Log4j2
-public class MemberServiceImpl implements MemberService {
-	
+public class MainServiceImpl  implements MainService{
+
 	@Autowired
-	private MemberDAO mapper;
-	
-	// 이메일로 회원 정보 가져오기
+	private MainDAO mapper;
+
+	// 메인 페이지 상품 리스트 조회
 	@Override
-	public MemberDTO getMemberInfo(String email) throws Exception {
-		return mapper.getMemberInfo(email);
-	}
-	
-	// 회원 수정
+	public List<MainListVO> getProductList(String pstatus, String category) throws Exception{
+		log.info("get Product List");
+		log.info(pstatus + " " + category);
+		return mapper.getProductList(pstatus, category);
+	};
+
+	// 메인 페이지 매거진 리스트 조회
 	@Override
-	public void modifyMember(String memail, String mpassword, String mname, Date birth, String telnum, String maddress, int gno) throws Exception {
-		mapper.modifyMember(memail, mpassword, mname, birth, telnum, maddress, gno);
-	}
-	
-	// 회원 탈퇴
-	@Override
-	public void delMember(String email) throws Exception {
-		mapper.delMember(email);
-	}
-	
-	// 회원 전체 조회
-	@Override
-	public List<MemberDTO> showAllMember() throws Exception {
-		return mapper.showAllMember();
+	public List<MainMagazineListVO> getMagazineList() throws Exception{
+		log.info("get Magazine List");
+		return mapper.getMagazineList();
 	}
 
-}
+}//end class
