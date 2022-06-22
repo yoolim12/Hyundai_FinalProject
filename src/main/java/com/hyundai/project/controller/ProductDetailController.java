226e562a89c1f4f3d66ae38575c0be4877eadd52
minketmon <gjm1026@naver.com>
2022-06-22 17:34:18 +0900

    get user email in detail

--- src/main/java/com/hyundai/project/controller/ProductDetailController.java
+++ src/main/java/com/hyundai/project/controller/ProductDetailController.java
@@ -1,6 +1,9 @@
 package com.hyundai.project.controller;
 
+import com.hyundai.project.dto.AuthMemberDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProductDetailController {
 
 	@GetMapping("/detail/{pid}")
-	public String productDetail(@PathVariable("pid") String pid) {
+	public String productDetail(@PathVariable("pid") String pid, @AuthenticationPrincipal AuthMemberDTO authMemberDTO, Model model) {
+		model.addAttribute("memberEmail", authMemberDTO.getMemail());
 		return "/product/detail";
 	}
 
