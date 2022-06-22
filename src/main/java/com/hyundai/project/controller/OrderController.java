4b003cb5f20d6ffb9d938dbf4817181a3778fcf6
minketmon <gjm1026@naver.com>
2022-06-22 17:30:11 +0900

    get user email in cart

--- src/main/java/com/hyundai/project/controller/OrderController.java
+++ src/main/java/com/hyundai/project/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.hyundai.project.dto.AuthMemberDTO;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
