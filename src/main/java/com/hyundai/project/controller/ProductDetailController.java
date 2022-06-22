82e7f937040ff9358621748a2e8de6dc8bf3a0b8
minketmon <gjm1026@naver.com>
2022-06-22 17:55:48 +0900

    get user email in order

--- src/main/java/com/hyundai/project/controller/ProductDetailController.java
+++ src/main/java/com/hyundai/project/controller/ProductDetailController.java
@@ -1,6 +1,7 @@
 package com.hyundai.project.controller;
 
 import com.hyundai.project.dto.AuthMemberDTO;
+import com.hyundai.project.dto.ClubAuthMemberDTO;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProductDetailController {
 
 	@GetMapping("/detail/{pid}")
-	public String productDetail(@PathVariable("pid") String pid, @AuthenticationPrincipal AuthMemberDTO authMemberDTO, Model model) {
-		model.addAttribute("memberEmail", authMemberDTO.getMemail());
+	public String productDetail(@PathVariable("pid") String pid, @AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model) {
+		if (oauthMemberDTO == null) {
+			model.addAttribute("memberEmail", authMemberDTO.getMemail());
+		}
+		else {
+			model.addAttribute("memberEmail", oauthMemberDTO.getEmail());
+		}
 		return "/product/detail";
 	}
 
