2442876ceb1d3986e76868e535723c2e0704db38
minketmon <gjm1026@naver.com>
2022-07-08 11:25:53 +0900

    detail GA

--- src/main/java/com/hyundai/project/controller/StreamingController.java
+++ src/main/java/com/hyundai/project/controller/StreamingController.java
@@ -1,6 +1,7 @@
 package com.hyundai.project.controller;
 
 import com.hyundai.project.dto.AuthMemberDTO;
+import com.hyundai.project.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -12,17 +13,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StreamingController {
 
 	@GetMapping("/streaming")
-	public void stream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, Model model){
+	public void stream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model) {
 
 		log.info("@StreamingController, GET()");
-		model.addAttribute("member", authMemberDTO);
+		if (oauthMemberDTO == null)
+			model.addAttribute("member", authMemberDTO);
+		else
+			model.addAttribute("member", oauthMemberDTO);
 
 	}
 	
 	@GetMapping("/mobile")
-	public void mobilestream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, Model model){
-		log.info("@StreamingController, GET()");
-		model.addAttribute("member", authMemberDTO);
-
+	public void mobilestream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model) {
+		log.info("@StreamingMobileController, GET()");
+		if (oauthMemberDTO == null)
+			model.addAttribute("member", authMemberDTO);
+		else
+			model.addAttribute("member", oauthMemberDTO);
 	}
 }
