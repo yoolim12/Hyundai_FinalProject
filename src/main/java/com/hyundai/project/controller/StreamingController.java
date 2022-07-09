e0940f39e9a8d7a6dc74e875eedc6cbc6f72fc09
ipepie <ansgurtk@naver.com>
2022-07-09 22:04:36 +0900

    mete delete

--- src/main/java/com/hyundai/project/controller/StreamingController.java
+++ src/main/java/com/hyundai/project/controller/StreamingController.java
@@ -16,7 +16,6 @@ public class StreamingController {
 	@GetMapping("/streaming")
 	public void stream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO,@AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
 
-		log.info("@StreamingController, GET()");
 		log.info(authMemberDTO);
 		log.info(oauthMemberDTO);
 		if (oauthMemberDTO == null)
@@ -27,7 +26,6 @@ public class StreamingController {
 	
 	@GetMapping("/mobile")
 	public void mobilestream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
-		log.info("@StreamingController, GET()");
 		if (oauthMemberDTO == null) {
         	model.addAttribute("member", authMemberDTO);
         }
