1405d5bc269b8ed8b1027b52856d40dc64dc8636
ipepie <ansgurtk@naver.com>
2022-07-10 15:45:14 +0900

    error page

--- src/main/java/com/hyundai/project/controller/StreamingController.java
+++ src/main/java/com/hyundai/project/controller/StreamingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StreamingController {
 
 	@GetMapping("/streaming")
-	public void stream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO,@AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
+	public String stream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO,@AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
 
 		log.info(authMemberDTO);
 		log.info(oauthMemberDTO);
@@ -22,20 +22,22 @@ public class StreamingController {
 			model.addAttribute("member", authMemberDTO);
 		else
 			model.addAttribute("member", oauthMemberDTO);
+		return "streaming/streaming";
 	}
 	
 	@GetMapping("/mobile")
-	public void mobilestream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
+	public String mobilestream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO, @AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
 		if (oauthMemberDTO == null) {
         	model.addAttribute("member", authMemberDTO);
         }
         else {
         	model.addAttribute("member", oauthMemberDTO);
         }
+		return "streaming/mobile";
 	}
 	
 	@GetMapping("/streamingReplay")
-	public void replaystream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO,@AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
+	public String replaystream(@AuthenticationPrincipal AuthMemberDTO authMemberDTO,@AuthenticationPrincipal ClubAuthMemberDTO oauthMemberDTO, Model model){
 
 		log.info("@StreamingController, GET()");
 		log.info(authMemberDTO);
@@ -44,5 +46,6 @@ public class StreamingController {
 			model.addAttribute("member", authMemberDTO);
 		else
 			model.addAttribute("member", oauthMemberDTO);
+		return "streaming/streamingReplay";
 	}
 }
